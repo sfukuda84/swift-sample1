3f567fd274d64774e456480bb50aba525ab398aa
fukudas <fukudas@fukudas-no-MacBook-Pro.local>
2014-10-31 11:57:08 +0900

    Initial Commit

---
+++ SwiftSample1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSample1
+//
+//  Created by fukudas on 2014/10/31.
+//  Copyright (c) 2014年 fukudas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
